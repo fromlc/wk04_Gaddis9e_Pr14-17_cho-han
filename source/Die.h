422ce303051f35ad49e5525fdd2eed26976b6772
L Carver <wifihum@gmail.com>
2023-02-17 21:53:19 -0800

    Add project files.

---
+++ source/Die.h
@@ -0,0 +1,19 @@
+//------------------------------------------------------------------------------
+// Die.h : class declaration/specification
+//------------------------------------------------------------------------------
+#ifndef DIE_H
+#define DIE_H
+
+class Die {
+private:
+   int sides;   // Number of sides
+   int value;   // The die's value
+
+public:
+   Die();					// Constructor
+   Die(int);				// Constructor
+   void roll();				// Rolls the die
+   int getSides() const;	// Returns the number of sides
+   int getValue() const;	// Returns the die's value
+};
+#endif
